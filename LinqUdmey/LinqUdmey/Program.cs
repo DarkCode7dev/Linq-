ddd52de77fc64582a440d5b44748d4a495c4d31b
Devesh puri goswami <weareoutsider1@outlook.com>
2020-07-01 23:46:47 +0530

    5 video 1 part

--- LinqUdmey/LinqUdmey/Program.cs
+++ LinqUdmey/LinqUdmey/Program.cs
@@ -8,60 +8,109 @@ namespace LinqUdmey
     {
         static void Main(string[] args)
         {
-            string sentence = "I love cats";
-
-            string[] catNames = { "Lucky", "Bella", "Luna", "Oreo", "Simba", "Loki", "Oscar", "Toby" };
-
-            int[] numbers = { 5, 6, 3, 2, 1, 5, 6, 7, 8, 4, 234, 54, 14, 653, 3, 4, 5, 6, 7 };
-
-            //"from" ke baad variable name phir "in" phir collection name phir "select" phir variable name 
-
-
-            var catswithA = from cat in catNames
-                            where cat.Contains("a")   
-                            where cat.Length <5
-                            select cat;
-
-
-
-
-            Console.WriteLine(string.Join(" ,", catswithA));
-
-
-
-
-
-
-
-
-
-
-
-
-
-            var getTheNumbers = from number in numbers
-                                where (number > 5) 
-                                where number  <  10
-                                orderby number descending 
-                                select number;
-
-
-            List<int> newNumbers = new List<int>();
-
-            foreach (var number in numbers)
+            List<Person> people = new List<Person>()
             {
-                if (number < 5)
-                {
-                    newNumbers.Add(number);
-                }
+
+                new Person("Tod", 180, 70, Gender.Male),
+                new Person("John", 170, 88, Gender.Male),
+                new Person("Anna", 150, 48, Gender.Female),
+                new Person("Kyle", 164, 77, Gender.Male),
+                new Person("Anna", 164, 77, Gender.Male),
+                new Person("Maria", 160, 55, Gender.Female),
+                new Person("John", 160, 55, Gender.Female)
+
+
+
+            };
+
+
+            var fourCharPeople = from p in people
+                                 where (p.Name.Length == 4)
+                                 orderby p.Weight
+                                 select p;
+
+            foreach (var item in fourCharPeople)
+            {
+                Console.WriteLine($" Name: {item.Name} , Weight : {item.Weight}");
             }
 
-
-            Console.WriteLine(string.Join("  , ", getTheNumbers));
-
-            //Console.WriteLine(string.Join(" ,", newNumbers));
         }
     }
 }
 
+internal class Person
+{
+    private string name;
+    private int height;
+    private int weight;
 
+    private Gender gender;
+
+    public string Name
+    {
+        get
+        {
+            return this.name;
+        }
+
+
+        set
+        {
+            this.name = value;
+        }
+    }
+
+
+   
+
+    public int Height
+    {
+        get
+        {
+            return this.height;
+        }
+
+
+        set
+        {
+            this.height = value;
+        }
+    }
+
+
+   
+    public int Weight
+    {
+        get
+        {
+            return this.weight;
+        }
+
+
+        set
+        {
+            this.weight = value;
+        }
+    }
+
+
+
+    public Gender Gender { get; set; }
+
+
+    public Person(string name , int height , int weight , Gender gender)
+    {
+        this.Name = name;
+        this.Height = height;
+        this.Weight = weight;
+        this.Gender = gender;
+
+    }
+
+}
+
+internal enum Gender
+{
+    Male,
+    Female
+}
